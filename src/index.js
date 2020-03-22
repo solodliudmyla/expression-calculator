4a98d0c48e7a1ec31dcae3ab015c50f105528f0d
Solod Liudmyla <solodludmila@gmail.com>
2020-03-22 17:54:11 +0200

    solve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-function eval() {
-    // Do not use eval!!!
-    return;
-}
-
 function expressionCalculator(expr) {
-    // write your solution here
+	if (/\/ 0/.test(expr)) {
+		throw new Error('TypeError: Division by zero.');
+	}
+	let bracketsLeft = expr.split('').filter(x => x === '(').join('');
+	let bracketsRight = expr.split('').filter(x => x === ')').join('');
+	if (bracketsLeft.length !== bracketsRight.length) {
+		throw new Error('ExpressionError: Brackets must be paired')
+	}
+	let calculator = new Function("return " + expr);
+	return calculator();
 }
 
 module.exports = {
-    expressionCalculator
-}
+	expressionCalculator
+};
